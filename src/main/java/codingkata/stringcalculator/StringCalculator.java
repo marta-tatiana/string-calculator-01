3583222571ba681c68d4559618297f098d5531a9
marta.tatiana <marta.tatiana>
2017-06-16 15:25:53 +0200

    supported defined delimeters

--- src/main/java/codingkata/stringcalculator/StringCalculator.java
+++ src/main/java/codingkata/stringcalculator/StringCalculator.java
@@ -1,19 +1,58 @@
 package codingkata.stringcalculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
+
+    private String regex = "//(.*?)\\\\n(.*)";
+    private String delimeter = ",";
+    private String inputs = null;
+
     public int add(String arguments) {
-        if (arguments.equals(""))
-            return 0;
-        return sum(arguments);
+        inputs = arguments;
+
+        return inputs.equals("") ? 0
+                : handleDelimeter().guardAgainstNewlines().sum();
     }
 
-    private int sum(String arguments) {
-        String[] numbers = arguments.split(",");
+    private StringCalculator handleDelimeter() {
+        if (isDelimeterProvided(inputs)) {
+            extractDelimeter(inputs);
+            inputs = cutOffDellimeterDefinition(inputs);
+        }
+        return this;
+    }
+
+    private StringCalculator guardAgainstNewlines() {
+        inputs = inputs.replace("\n", delimeter);
+        return this;
+    }
+
+    private void extractDelimeter(String arguments) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(arguments);
+        matcher.find();
+        delimeter = matcher.group(1);
+    }
+
+    private int sum() {
+        String[] numbers = inputs.split(delimeter);
         int acc = 0;
         for (String number : numbers) {
             acc += Integer.parseInt(number);
         }
         return acc;
     }
+
+
+    boolean isDelimeterProvided(String arguments) {
+        return arguments.matches(regex);
+    }
+
+    String cutOffDellimeterDefinition(String arguments) {
+        return arguments.substring(delimeter.length() + 4);
+    }
+
 
 }
--- src/test/java/codingkata/stringcalculator/StringCalculatorTTest.java
+++ src/test/java/codingkata/stringcalculator/StringCalculatorTTest.java
@@ -2,6 +2,8 @@ package codingkata.stringcalculator;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -33,6 +35,31 @@ public class StringCalculatorTTest {
     @Test public void
     should_sum_two_numbers() {
         assertThat(calculator.add("5,6"), is(11));
+    }
+
+    @Test public void
+    should_sum_many_numbers() {
+        assertThat(calculator.add("5,8,2,3,6,1,-1,0"), is(24));
+    }
+
+    @Test public void
+    should_allow_for_newlines_as_delimeters() {
+        assertThat(calculator.add("7\n1,2"), is(10));
+    }
+
+    @Test public void
+    should_recognize_when_delimeter_is_provided() {
+        assertThat(calculator.isDelimeterProvided("//;\\n1;5;6;7;-1;-5;-6"), is(true));
+    }
+
+    @Test public void
+    shoud_cut_off_delimeter_definition() {
+        assertThat(calculator.cutOffDellimeterDefinition("//;\\n1;5;6;7;-1;-5;-6"), equalTo("1;5;6;7;-1;-5;-6"));
+    }
+
+    @Test public void
+    should_work_with_provided_delimeter() {
+        assertThat(calculator.add("//;\\n1;5;6;7;-1;-5;-6"), is(7));
     }
 
 }
